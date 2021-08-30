c05e260b5167089bbfe48bae6bc0f76b892e9dbd
sairajzero <sairaj0630@gmail.com>
2021-08-30 22:00:31 +0530

    Adding std_op

---
+++ src/floGlobals.js
@@ -0,0 +1,16 @@
+const floGlobals = {
+
+    //Required for all
+    blockchain: "FLO",
+
+    //Required for blockchain API operators
+    apiURL: {
+        FLO: ['https://livenet.flocha.in/', 'https://flosight.duckdns.org/'],
+        FLO_TEST: ['https://testnet-flosight.duckdns.org', 'https://testnet.flocha.in/']
+    },
+    //sendAmt: 0.001,
+    //fee: 0.0005,
+};
+
+(typeof global !== "undefined" ? global : window).cryptocoin = floGlobals.blockchain;
+('object' === typeof module) ? module.exports = floGlobals : null;
